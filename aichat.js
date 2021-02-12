65b4e403b8c3e0b2484cb5c06d578860326660f1
Mahesh Bansod <mahesh0bansod@gmail.com>
2021-02-12 15:17:17 +0530

    added call to lydia api

--- aichat.js
+++ aichat.js
@@ -1,4 +1,47 @@
+const axios = require('axios');
+const db = require('./db.js').db
+      var FormData = require('form-data');
+const BOT_API_TOKEN = process.env.BOT_API;
+const ch_accesskey = process.env.CH_ACCESSKEY; //coffee house access key
 
 exports.replyto = async function(msg) {
-    return "bruh";
+
+  var dbref = db.ref('chats/'+msg.chat.id+'/aichat/'+msg.chat.id+'/user/'+msg.from.id);
+  var snapshot = await dbref.once('value');
+  var sessionId;
+  var t_res = 0;
+  if(!snapshot.exists()) {
+  	var response = await axios.post('https://api.intellivoid.net/coffeehouse/v1/lydia/session/create');
+  	if(!response.data.success) {
+  		console.log(response.data.error);
+  		return;
+  	}
+  	sessionId = response.data.results.session_id;
+  	var dbref = db.ref('chats/'+msg.chat.id+'/aichat/user/'+msg.from.id);
+  	dbref.set({sessionId: sessionId});
+  } else {
+  	sessionId = snapshot.val().sessionId;
+  }
+  
+	var data = new FormData();
+	data.append('access_key', ch_accesskey);
+	data.append('session_id', sessionId);
+	data.append('input', msg.text);
+	
+	var config = {
+		method: 'post',
+		url: 'https://api.intellivoid.net/coffeehouse/v1/lydia/session/think',
+		headers: { 
+		...data.getHeaders()
+		},
+		data : data
+	};
+	var response = await axios(config);
+	if(!response.data.success) {
+		console.log(response.data.error);
+		return;
+	}
+	var output = response.data.results.output;
+	
+	return output;
 }

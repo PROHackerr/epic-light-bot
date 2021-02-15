0d0237a5015e6c1d4d92ca1e0d2c4fd87abc1376
Mahesh Bansod <mahesh0bansod@gmail.com>
2021-02-15 13:34:59 +0530

    added warnsystem skeleton

---
+++ warnsystem.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const db = require('./db.js').db
+const BOT_API_TOKEN = process.env.BOT_API;
+const ch_accesskey = process.env.CH_ACCESSKEY; //coffee house access key
+
+exports.addwarn = async function(user, chat, reason, warn3action) {
+	var dbref = db.ref("chats/"+chat.id+"/warns/"+user.id);
+	var snapshot = await dbref.once("value");
+	var nw = 0;
+	if(snapshot.exists()) {
+		warnlist = snapshot.val();
+		var nw = warnlist.length; //no of warns
+		if(nw >= 2) {
+			//send message about all warns and do warn3action
+			return;
+		}
+	}
+	warnlist.push({reason: reason});
+	
+	dbref.set(warnlist);
+	
+	//send message about the currect warn with reason
+}
+

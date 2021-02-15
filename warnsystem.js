333e016952f9dfd396c587f7f8c726ce207f1d02
Mahesh Bansod <mahesh0bansod@gmail.com>
2021-02-15 15:47:23 +0530

    defined warnlist

--- warnsystem.js
+++ warnsystem.js
@@ -9,7 +9,7 @@ exports.addwarn = async function(user, chat, reason, warn3action) {
 	var snapshot = await dbref.once("value");
 	var nw = 0;
 	if(snapshot.exists()) {
-		warnlist = snapshot.val();
+		var warnlist = snapshot.val();
 		var nw = warnlist.length; //no of warns
 		if(nw >= 2) {
 			//send message about all warns and do warn3action
